9716f303813e08b27b332758e87df4a97ec4e5a7
wtdavis <wtdavis@wesleyan.edu>
2024-05-19 23:54:56 -0400

    add query selectors and event listeners

--- portfolio.js
+++ portfolio.js
@@ -1,7 +1,10 @@
-
+window.addEventListener("DOMContentLoaded", console.log("dom content loaded"))
+window.addEventListener("click", console.log("window click"))
 
 
 function App () {
+    console.log("dom content loaded")
+    window.addEventListener("click", console.log("click"))
     let document = new Document()
     let tile1 = document.getElementById("tile-1")
     // let tile1 = document.querySelector(".tile-1")
@@ -18,16 +21,19 @@ function App () {
     tile1.addEventListener("click", () => {
         // currentTile = tile1
         // productScreen = true
+        console.log("click")
         handleTileClick(tile1) 
     })
     tile2.addEventListener("click", () => {
         // currentTile = tile2
         // productScreen = true
+        console.log("click")
         handleTileClick(tile2) 
     })
     tile3.addEventListener("click", () => {
         // currentTile = tile3
         // productScreen = true
+        console.log("click")
         handleTileClick(tile3) 
     })
 
@@ -42,5 +48,3 @@ function App () {
         console.log(currentTile)
     }
 }
-
-window.addEventListener("DOMContentLoaded", App())
