71ebb7ced4bd1a9f588bd6aa952cf6c2b4da349e
wtdavis <wtdavis@wesleyan.edu>
2024-05-26 21:11:16 -0400

    product tile morph

---
+++ products.js
@@ -0,0 +1,7 @@
+function Product () {
+    let tile1 = document.querySelector(".tile1")
+    let tile2 = document.querySelector(".tile2")
+    let tile3 = document.querySelector(".tile3")
+}
+
+export default Product
