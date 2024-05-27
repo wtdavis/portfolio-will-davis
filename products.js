4adf9ca6f9cdaca857520bd9299e7865903827a7
wtdavis <wtdavis@wesleyan.edu>
2024-05-27 08:47:49 -0400

    css updates

--- products.js
+++ products.js
@@ -8,6 +8,8 @@ function Product (activeTile) {
     let tile2Img = document.querySelector(".tile-2-img")
     let tile3Img = document.querySelector(".tile-3-img")
 
+    let productPanel = document.querySelector(".product-screen")
+
 
     let enlargeTile;
     let normalsizeTiles;
@@ -44,6 +46,7 @@ function Product (activeTile) {
         for (let i=0; i<normalsizeTiles.length; i++) {
             normalsizeTiles[i].setAttribute("style", "height:140px")
         }
+
     }
 
 }
